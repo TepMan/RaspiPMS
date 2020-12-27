1db374e0fcca6dfb61ccb0eed13be94e900a5c30
Andreas Tepper <tepper.andreas@gmail.com>
2020-12-27 21:52:09 +0100

    Gitignore file added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
 
 namespace RaspiPMS
 {
@@ -6,7 +9,33 @@ namespace RaspiPMS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                using SerialPort sp = new SerialPort("/dev/serial0");
+                sp.Encoding = Encoding.UTF8;
+                sp.BaudRate = 9600;
+                sp.ReadTimeout = 1000;
+                sp.WriteTimeout = 1000;
+                sp.Open();
+
+                bool finished = false;
+                Console.CancelKeyPress += (a, b) =>
+                {
+                    finished = true;
+                    // close port to kill pending operations
+                    sp.Close();
+                };
+
+                while(!finished)
+                {
+                    Console.WriteLine("Warte auf Signale...");
+                    Thread.Sleep(30000);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Es ist ein Fehler aufgetreten: " + ex.ToString());
+            }
         }
     }
 }
